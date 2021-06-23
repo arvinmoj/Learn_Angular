7a4e3ad950311194001ad6f51de1c3f35c731b35
Arvin <arvinmojaverain@hotmail.com>
2021-06-23 21:36:31 +0430

    Array , For , If , Hidden

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,10 +16,42 @@ import { Component } from "@angular/core";
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   public constructor() {
+    
     this.name = `Arvinmoj`;
+    this.number1 = 2000;
+    this.number2 = 2000;
+
+
+    this.fullNames =[];
+    this.fullNames.push(`Ali`);
+    this.fullNames.push('Pouria');
+    this.fullNames.push('Shervin');
+    this.fullNames.push(`Arvinmoj`);
+
+    this.usernames=[`Username`];
+    this.usernameCount = 10;
+    for (let index: number = 0; index < this.usernameCount; index++) {
+      this.usernames.push(`Username`+ " " + index)
+    }
+
+    this.display = false;
+    this.hide = true;
+
   }
 
-  public name: string
-}
+  public name: string;
 
+  public number1: number;
+  public number2: number;
+  
+  public fullNames: string[];
+  public usernames: string[];
+  
+  public usernameCount: number;
+
+  public display: boolean;
+  public hide: boolean;
+
+}
